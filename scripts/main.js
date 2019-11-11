97b23bdfac1b388302219f439e25524829b6cfbf
theun0524 <43725818+theun0524@users.noreply.github.com>
2019-11-11 13:11:13 +0900

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -2,8 +2,8 @@ var button = document.getElementById('test-button');
 
 button.addEventListener('click', function(e) {
   var form = document.getElementById("mooc");
-  document.getElementById('token').value = 'rfXtRQlS/kvURh6X9gQ1sA==';
-  document.getElementById('token2').value = '%EC%9E%A5%ED%9D%AC%EC%A4%80';
+  document.getElementById('token').value = 'b7LhlGBIMQ4aULCg0t8f8w==';
+  document.getElementById('token2').value = '%ED%85%8C%EC%8A%A4%ED%8A%B8%EC%95%84%EC%9D%B4%EB%94%94%EB%94%94';
   //form.action="https://st-mooc-pos.xinics.kr/imoove-auth-client/km-link-integrated";
   //form.action="https://www.postechx.kr/imoove-auth-client/km-link-integrated";
   form.action="https://kt.dev.smartlearn.kr";
