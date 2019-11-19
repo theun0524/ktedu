39bf75e2184d87362fb87cc58e5b64e75cee495a
theun0524 <43725818+theun0524@users.noreply.github.com>
2019-11-19 13:27:49 +0900

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -6,7 +6,7 @@ button.addEventListener('click', function(e) {
   document.getElementById('token2').value = '%ED%85%8C%EC%8A%A4%ED%8A%B8%EC%95%84%EC%9D%B4%EB%94%94%EB%94%94';
   //form.action="https://st-mooc-pos.xinics.kr/imoove-auth-client/km-link-integrated";
   //form.action="https://www.postechx.kr/imoove-auth-client/km-link-integrated";
-  form.action="https://kt.dev.smartlearn.kr";
+  form.action="https://kt.dev.smartlearn.io";
   form.target="_blank";
   form.submit();
 });
